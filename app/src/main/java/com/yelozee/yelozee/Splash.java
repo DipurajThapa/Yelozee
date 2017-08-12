ac72253cdb002b7aa88934d1830b67d43b7ddd09
Thapa <Dipuraj_Thapa@amer.dell.com>
2017-08-12 17:22:17 +0530

    Initial commit

---
+++ app/src/main/java/com/yelozee/yelozee/Splash.java
@@ -0,0 +1,38 @@
+package com.yelozee.yelozee;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
+
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread myThread = new Thread(){
+            public void run(){
+
+                try{
+
+                    sleep(3000);
+                    Intent intent= new Intent(getApplicationContext(),Register.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+
+                }
+
+            }
+
+
+        };myThread.start();
+
+
+    }
+}
